5aa2db98fe973b1c80a57c844dc6d8e04a9e57ee
Slater Meehan <slatermeehan@gmail.com>
2025-06-12 22:14:44 -0400

    Fix ArrowRight import in blog articles

--- app/blog/first-blog/page.js
+++ app/blog/first-blog/page.js
@@ -1,6 +1,6 @@
 'use client';
 import React from 'react';
-import { ArrowLeft, Clock, User } from 'lucide-react';
+import { ArrowLeft, Clock, ArrowRight } from 'lucide-react';
 import Link from 'next/link';
 
 const BlogPage = () => {
